c02daa47650cba633f8aeb1d6ed556af7f5b7e1f
LucasFeli <felipe298@gmail.com>
2022-10-19 16:56:20 +0200

    add function to buttom 4

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ const boton1 = () =>{
 }
 
 const boton2 = () =>{
+    const element= document.querySelector('#container')
     const imagen= document.createElement('IMG');
     imagen.setAttribute('src','wraith.webp');
     imagen.setAttribute('width','304');
     imagen.setAttribute('height','228');
     imagen.setAttribute('alt','Rene Blasey');
-    document.body.appendChild(imagen);
+    element.appendChild(imagen);
     
 }
 
 const boton3 = () =>{
+    const element= document.querySelector('#container')
     const boton = document.createElement('BUTTON');
     const textoBoton = document.createTextNode('boton')
     boton.appendChild(textoBoton)
-    document.body.appendChild(boton);
+    element.appendChild(boton);
+    
+}
+
+
+const boton4 = () =>{
+    const borrar = document.getElementById('container')
+    borrar.removeChild(borrar.firstChild);
     
 }
